9809bf7ee236958565377bbc4cc800b1f9458816
Shariar Joy <shariarjoy707@gmail.com>
2026-07-23 01:33:53 +0600

    some little update

--- frontend/uni_marketplace/src/main.tsx
+++ frontend/uni_marketplace/src/main.tsx
@@ -8,3 +8,21 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
+
+
+// cd C:\Users\USER\dynamodb-local
+// java -jar DynamoDBLocal.jar -port 8001 -dbPath .
+
+
+
+
+// cd backend
+// .venv/Scripts/python.exe -m uvicorn app.main:app --reload --port 8080
+
+
+
+
+// cd frontend/uni_marketplace
+// npm run dev
